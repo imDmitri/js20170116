40bf0abc16393f6375ed113234115b2542ee3c8f
Dmitri Ilin <quarrazzella@gmail.com>
2017-01-20 16:10:07 +0300

    Update and rename exercises.html to tests.js

---
+++ scripts/tests.js
@@ -0,0 +1,15 @@
+function pow(x, n) {
+	
+	//var result = 1;
+	var	checkDegree = n > 0 && Number.isInteger(n);
+
+	function raiseNumber(){
+		var result = 1
+  		for (var i = 0; i < n; i++) {
+    		result *= x;
+  		}
+  		return result;
+  	}
+
+  	return (checkDegree) ? raiseNumber() : NaN;
+}
