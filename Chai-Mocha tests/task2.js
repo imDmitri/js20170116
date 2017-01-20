38adc00a3b5d39550173854f7c7d180761590841
Dmitri Ilin <quarrazzella@gmail.com>
2017-01-20 19:25:21 +0300

    Update task2.js

--- Chai-Mocha tests/task2.js
+++ Chai-Mocha tests/task2.js
@@ -35,9 +35,7 @@ function pow(x, n) {
 */
 
 describe("pow", function() {
-
 	describe("возводит x в степень n", function() {
-
 		function makeTest(x) {
 		  var expected = x * x * x;
 		  it("при возведении " + x + " в степень 3 результат: " + expected, function() {
@@ -49,16 +47,20 @@ describe("pow", function() {
 		  makeTest(x);
 		}
 	});
-
-	it("при возведении в отрицательную степень результат NaN", function() {
-		assert(isNaN(pow(2, -1)), "pow(2, -1) возвращает NaN");
+	
+	describe ("случаи, когда функция возвращает NaN", function(){
+		it("при возведении в отрицательную степень результат NaN", function() {
+			assert(isNaN(pow(2, -1)), "pow(2, -1) возвращает NaN");
+		});
+		it("при возведении в дробную степень результат NaN", function() {
+			assert(isNaN(pow(2, 1.5)), "pow(2, -1.5) возвращает NaN");
+		});
+		it("ноль в нулевой степени равно NaN", function() {
+			assert(isNaN(pow(0, 0)), "pow(0, 0) возвращает NaN");
+		});
 	});
 
-	it("при возведении в дробную степень результат NaN", function() {
-		assert(isNaN(pow(2, 1.5)), "pow(2, -1.5) возвращает NaN");
-	});
-
-	describe("любое число, кроме нуля, в нулевой степени равно 1", function(){
+	describe("любое число, кроме нуля, в степени 0 равно 1", function(){
 		function makeTest(x) {
 	  		it("возведение " + x + "в нулевую степень дает 1", function() {
 	    		assert.equal(pow(x, 0), 1);
@@ -69,9 +71,4 @@ describe("pow", function() {
 		  makeTest(x);
 		}
 	});
-
-	it("ноль в нулевой степени равно NaN", function() {
-		assert(isNaN(pow(0, 0)), "pow(0, 0) возвращает NaN");
-	});
-
 });
