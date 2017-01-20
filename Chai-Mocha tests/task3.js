75768c6c7fb53928299022927b20da5da7854f55
Dmitri Ilin <quarrazzella@gmail.com>
2017-01-20 20:21:06 +0300

    Create task3.js

---
+++ Chai-Mocha tests/task3.js
@@ -0,0 +1,20 @@
+/*
+мой вариант содержания test.js с использованием цикла 
+*/
+
+describe("pow", function() {
+	describe("Возводит x в степень n", function() {
+
+		function test(x){
+			var result = (x === 1) ? 5 : (x === 2) ? 25 : (x === 3) ? 125 :
+			false;
+			it("5 в степени " + x + " равно " + result, function() {
+	    		assert.equal(pow(5, x), result);
+	  		});
+		}
+
+		for (var i = 1; i <= 3; i++){
+			test(i);
+		}
+	});
+});
