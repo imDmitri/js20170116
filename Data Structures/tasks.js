98812f206445179a567b503f354c8d3acbbbe0d4
Dmitri Ilin <quarrazzella@gmail.com>
2017-01-25 21:13:21 +0300

    Update tasks.js

--- Data Structures/tasks.js
+++ Data Structures/tasks.js
@@ -15,10 +15,10 @@ function getDecimal(num) {
 
 // мое решение
 
-function getDecimal(x) {
-	var isNumeric = (!isNaN(parseFloat(x)) && isFinite(x)) ? true : false;
-	if (!isNumeric) return "error: function's argument must be numeric";
-	if (x.toString().indexOf('.') === -1) return 0;
-  
-	return +(0 + '.' + x.toString().split('.')[1]);
+function getDecimal(num) {
+	var isNumeric = (!isNaN(parseFloat(num)) && isFinite(num)) ? true : false; // проверяем на число
+	if (!isNumeric) return "error: function's argument must be numeric"; // возвращаем ошибку в случае, если аргумент - не число
+	if (num.toString().indeхOf('.') === -1) return 0; // проверяем, дробное ли число
+
+	return +(0 + '.' + num.toString().split('.')[1]);
 }
