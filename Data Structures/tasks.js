bc0f8a55049a347c057a3c3fd19d840ae61c02bf
Dmitri Ilin <quarrazzella@gmail.com>
2017-01-25 21:09:05 +0300

    Update tasks.js

--- Data Structures/tasks.js
+++ Data Structures/tasks.js
@@ -1,1 +1,24 @@
-// код здесь
+/*
+Задача:
+Напишите функцию getDecimal(num), которая возвращает десятичную часть числа:
+*/
+
+// Решение в учебнике
+
+function getDecimal(num) {
+  var str = "" + num;
+  var zeroPos = str.indexOf(".");
+  if (zeroPos == -1) return 0;
+  str = str.slice(zeroPos);
+  return +str;
+}
+
+// мое решение
+
+function getDecimal(x) {
+	var isNumeric = (!isNaN(parseFloat(x)) && isFinite(x)) ? true : false;
+	if (!isNumeric) return "error: function's argument must be numeric";
+	if (x.toString().indexOf('.') === -1) return 0;
+  
+	return +(0 + '.' + x.toString().split('.')[1]);
+}
